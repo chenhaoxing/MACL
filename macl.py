82d3fb51d983fde6ea94fdf2d8ae47d751094ba3
chx_nju <58558762+chenhaoxing@users.noreply.github.com>
2023-04-26 13:42:00 +0800

    Update macl.py

--- macl.py
+++ macl.py
@@ -1,44 +1,24 @@
 import torch
 
-class MACLa(object):
+# MACL temperature strategy with reweighing
 
-    def __init__(self, tau_init=0.1, alpha=2.0):
-        super(MACLa, self).__init__()
-        self.tau_init = tau_init
-        self.alpha = alpha
-
-    def __call__(self, fx, gx):
-        logits = fx @ gx.T
-        A=(torch.trace(logits)/fx.shape[0]).detach()
-        tau = self.tau_init * self.alpha**A
-        P = torch.softmax(logits/tau, dim=1)
-
-        mask = torch.ones(P.shape, device=fx.device).fill_diagonal_(0).bool()
-        W0 = (P.masked_select(mask)).view(P.shape[0],-1).sum(-1)
-        W1 = torch.ones(fx.shape[0], device=fx.device)
-        W = W1 / W0
-
-        loss = -W.detach() * torch.log(P).diag()
-        return loss.mean()
-
-
-class MACLb(object):
-
-    def __init__(self, tau_init=0.1, beta=1.0):
-        super(MACLb, self).__init__()
-        self.tau_init = tau_init
-        self.beta = beta
-
-    def __call__(self, fx, gx):
-        logits = fx @ gx.T
-        A=(torch.trace(logits)/fx.shape[0]).detach()
-        tau = self.tau_init*(1 + self.beta*A)
-        P = torch.softmax(logits/tau, dim=1)
-
-        mask = torch.ones(P.shape, device=fx.device).fill_diagonal_(0).bool()
-        W0 = (P.masked_select(mask)).view(P.shape[0],-1).sum(-1)
-        W1 = torch.ones(fx.shape[0], device=fx.device)
-        W = W1 / W0
-
-        loss = -W.detach() * torch.log(P).diag()
-        return loss.mean()
+# pos: positive representation similarities, Nx1
+# neg: negative representation similarities, NxK
+# tau_0: basic temperature
+# alpha: scaling factor
+# A0: initial alignment threshold
+    
+def MACL(pos, neg, tau_0, alpha, A0):
+    
+    # alignment-adaptive temperature
+    A=torch.mean(pos.detach())
+    tau = tau_0 * (1. + alpha*(A-A0))
+    
+    logits = torch.cat([pos, neg], dim=1) / tau  
+    P = torch.softmax(logits, dim=1)[:,0]
+    
+    # reweighting and loss computation
+    V = 1. / (1.- P)
+    loss = -V.detach() * torch.log(P)
+    
+    return loss.mean()
